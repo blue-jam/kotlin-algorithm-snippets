06ff98067e288a0b3fc79ba397f24845d1703479
blue-jam <blue-jam@users.noreply.github.com>
2019-10-12 15:41:47 +0900

    Change method name in test

--- src/test/kotlin/bluejam/math/modint/AojNtl1B.kt
+++ src/test/kotlin/bluejam/math/modint/AojNtl1B.kt
@@ -4,12 +4,12 @@ import bluejam.math.ModInt
 
 val MOD = 1000000007L
 
-fun solve(m: Long, n: Long): Long {
+fun solveAojNtl1B(m: Long, n: Long): Long {
     return ModInt(m, MOD).pow(n).value
 }
 
 fun main(args: Array<String>) {
     val (m, n) = readLine()!!.split(" ").map { it.toLong() }
 
-    println(solve(m, n))
+    println(solveAojNtl1B(m, n))
 }
