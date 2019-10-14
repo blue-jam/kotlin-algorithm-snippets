5756de969546baeb5a00383301d1529a8c07a15c
blue-jam <blue-jam@users.noreply.github.com>
2019-10-14 17:00:06 +0900

    Enable test

--- src/test/kotlin/bluejam/math/gcd/AojNtl1C.kt
+++ src/test/kotlin/bluejam/math/gcd/AojNtl1C.kt
@@ -1,14 +1,33 @@
 package bluejam.math.gcd
 
 import bluejam.math.lcm
+import bluejam.testutils.createScannerForCase
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTimeout
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
+import java.io.File
+import java.time.Duration
+import java.util.*
 
-fun solveAojNtl1C(a: List<Long>): Long {
-    return a.fold(1L) { x, y -> lcm(x, y)}
+fun solveAojNtl1C(sc: Scanner): Long {
+    val N = sc.nextInt()
+    val a = Array(N) { sc.nextLong() }
+
+    return a.fold(1L, ::lcm)
 }
 
-fun main(args: Array<String>) {
-    val N = readLine()!!.toInt()
-    val a = readLine()!!.split(" ").map { it.toLong() }
+class AojNtl1C {
+    @TestFactory
+    fun run(): List<DynamicTest> {
+        val directory = File("build/test-cases/aoj/NTL_1_C")
 
-    println(solveAojNtl1C(a))
+        return directory.listFiles { file -> file.extension == "in" }!!
+            .map { DynamicTest.dynamicTest(it.nameWithoutExtension) {
+                val (inSc, diffSc) = createScannerForCase(it)
+                val res = assertTimeout<Long>(Duration.ofSeconds(1)) { solveAojNtl1C(inSc) }
+
+                assertEquals(diffSc.nextLong(), res)
+            } }
+    }
 }
--- src/test/kotlin/bluejam/math/modint/AojNtl1B.kt
+++ src/test/kotlin/bluejam/math/modint/AojNtl1B.kt
@@ -1,15 +1,33 @@
 package bluejam.math.modint
 
 import bluejam.math.ModInt
+import bluejam.testutils.createScannerForCase
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.TestFactory
+import java.io.File
+import java.time.Duration
+import java.util.*
 
 val MOD = 1000000007L
 
-fun solveAojNtl1B(m: Long, n: Long): Long {
+fun solveAojNtl1B(sc: Scanner): Long {
+    val m = sc.nextLong()
+    val n = sc.nextLong()
     return ModInt(m, MOD).pow(n).value
 }
 
-fun main(args: Array<String>) {
-    val (m, n) = readLine()!!.split(" ").map { it.toLong() }
+class AojNtl1B {
+    @TestFactory
+    fun run(): List<DynamicTest> {
+        val directory = File("build/test-cases/aoj/NTL_1_B")
 
-    println(solveAojNtl1B(m, n))
+        return directory.listFiles { file -> file.extension == "in" }!!
+            .map { DynamicTest.dynamicTest(it.nameWithoutExtension) {
+                val (inSc, diffSc) = createScannerForCase(it)
+                val res = Assertions.assertTimeout<Long>(Duration.ofSeconds(1)) { solveAojNtl1B(inSc) }
+
+                Assertions.assertEquals(diffSc.nextLong(), res)
+            } }
+    }
 }
