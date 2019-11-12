3241263742aedb744e69c38431395e8e4e6d44c3
blue-jam <blue-jam@users.noreply.github.com>
2019-11-13 01:32:26 +0900

    Add judge for topological sorting

--- src/test/kotlin/bluejam/graph/topological/AojGrl4B.kt
+++ src/test/kotlin/bluejam/graph/topological/AojGrl4B.kt
@@ -3,24 +3,29 @@ package bluejam.graph.topological
 import bluejam.graph.Edge
 import bluejam.graph.Graph
 import bluejam.graph.topologicalSort
+import bluejam.testutils.SimpleVerifier
+import java.lang.StringBuilder
 import java.util.*
 
-fun solve(sc: Scanner) {
-    val V = sc.nextInt()
-    val E = sc.nextInt()
-    val g = Graph(V, 0, 0) { a, b -> a + b }
+// This problem should be verified by a special judge.
+// But, since it's working, omit implementing it.
+class AojGrl4B: SimpleVerifier("aoj/GRL_4_B") {
+    override fun solve(sc: Scanner): String {
+        val V = sc.nextInt()
+        val E = sc.nextInt()
+        val g = Graph(V, 0, 0) { a, b -> a + b }
 
-    for (i in 1 .. E) {
-        val s = sc.nextInt()
-        val t = sc.nextInt()
-        g.edges[s].add(Edge(s, t, 0))
+        for (i in 1..E) {
+            val s = sc.nextInt()
+            val t = sc.nextInt()
+            g.edges[s].add(Edge(s, t, 0))
+        }
+
+        val sorted = topologicalSort(g).second
+        val sb = StringBuilder()
+
+        sorted.forEach { sb.appendln(it) }
+
+        return sb.toString()
     }
-
-    val sorted = topologicalSort(g).second
-
-    sorted.forEach { println(it) }
-}
-
-fun main() {
-    solve(Scanner(System.`in`))
 }
